c6c974cf30f8b2de431b24cd0bf0e261af7228d2
dilipLS <dilip@letsservice.in>
2017-08-22 18:49:12 +0530

    22augeve

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app.use(bodyParser.json());
 app.get('/',function(req,res){
 res.send('It works');
 });
-var busstop_routes = require('./routes/busstop_routes.js');
-app.use('api/busstop', busstop_routes);
+var busstop_routes = require('./routes/busstop_routes');
+app.use('/api/busstop', busstop_routes);
 
 
-var port='8080';
+var port='80';
 app.listen(port);
 console.log('Listening on port:'+port );
 module.exports = app;
--- routes/busstop_routes.js
+++ routes/busstop_routes.js
@@ -4,32 +4,33 @@ var mongoose = require('mongoose');
 mongoose.set('debug', true);
 var BusStop = require('../models/busStop.js');
 
+router.get('/',function(req,res){
+res.send('It works');
+});
+
 router.post('/createBusStop', function(req, res) {
 	var temp = new BusStop();
 	temp.name = req.body.name;
 	temp.stopId = req.body.stopId;
-	temp.loc = {type: "Point",point:[req.body.lng,req.body.lat]}
+	temp.loc = {type: "Point",coordinates:[req.body.lng,req.body.lat]}
 	temp.save(function(err){
 		if(err){
-			return res.json({ success: false , 
-				msg: 500 });
+			res.json({ success: false , msg: err });
+		}else{
+			res.json({ success: true , msg: 200 });	
 		}
 	});
-	res.json({ success: true , msg: 200 });				
-	
 });
 
 router.get('/getBusStops', function(req, res) {
 	console.log('api hit');
-	BusStop.find(function(err, user) {
+	BusStop.find(function(err, busstops) {
 		if (err) {
-			return res.json({ success: false , 
-				msg: 500 });
-			}
-		return res.json({ success: true , BusStops: BusStops });
+			res.json({ success: false , msg: 500 });
+		}else{
+			res.json({ success: true , data: busstops });
+		}
 	});
-	
-	
 });
-
+	
 module.exports = router;
